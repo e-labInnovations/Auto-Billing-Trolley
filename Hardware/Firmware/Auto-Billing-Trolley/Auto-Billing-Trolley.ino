712b01bcf1599888fca64df7c429aadcc9efe61c
e-labInnovations <mohammedashad23@gmail.com>
2023-05-10 20:57:58 +0530

    added necessary comments

--- Hardware/Firmware/Auto-Billing-Trolley/Auto-Billing-Trolley.ino
+++ Hardware/Firmware/Auto-Billing-Trolley/Auto-Billing-Trolley.ino
@@ -1,5 +1,5 @@
 #include <Wire.h>
-#include "src/LiquidCrystal_I2C/LiquidCrystal_I2C.h"  //https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library
+#include "src/LiquidCrystal_I2C/LiquidCrystal_I2C.h"  // Library for I2C LCD display
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
 #include <Hash.h>
@@ -9,108 +9,121 @@
 #include <WiFiManager.h>
 
 /*
-Libraries Using
-  EspSoftwareSerial - version 8.0.1 
-  ESP8266WiFi       - version 1.0
-  Hash              - version 1.0 
-  WebSockets        - version 2.3.6
-  ArduinoJson       - version 6.20.1 
-  WiFiManager       - version 2.0.15-rc.1 
-  ESP8266WebServer  - version 1.0 
-  DNSServer         - version 1.1.1 
+Libraries using:
+- EspSoftwareSerial - version 8.0.1 
+- ESP8266WiFi - version 1.0
+- Hash - version 1.0 
+- WebSockets - version 2.3.6
+- ArduinoJson - version 6.20.1 
+- WiFiManager - version 2.0.15-rc.1 
+- ESP8266WebServer - version 1.0 
+- DNSServer - version 1.1.1 
 */
 
-// define the pins used for the software serial connection
+// Define the pins used for the software serial connection
 #define RFID_Serial_TX 12
 #define RFID_Serial_RX 13
 
-// define the pins used for the remove item button
+// Define the pins used for the remove item button
 #define REMOVE_Item_Btn 14
 
-//
+// Define the I2C address of the LCD display
 #define DISPLAY_ADDR 0x3F
 
-//
+// Define the path for the JSON configuration file
 #define JSON_CONFIG_FILE "/config.json"
-//
-#define SSID "Auto Billing Trolley"
-#define PASSWORD    "password"
 
-// create a new instance of the EspSoftwareSerial library
+// Define the SSID and password for the WiFi network
+#define SSID "Auto Billing Trolley"
+#define PASSWORD "password"
+
+// Create a new instance of the EspSoftwareSerial library
 EspSoftwareSerial::UART RFID_Serial;
 
-//
+// Initialize an instance of the LiquidCrystal_I2C library for the LCD display
 LiquidCrystal_I2C lcd(DISPLAY_ADDR, 16, 2);
 
-//
+// Initialize an instance of the WiFiManager library for managing the WiFi connection
 WiFiManager wm;
-//
+
+// Initialize an instance of the WebSocketsClient library for communicating with a WebSocket server
 WebSocketsClient webSocket;
 
-// initialize variables used to store the card number
+// Initialize variables used to store the card number
 int count = 0;
 char card_no[12];
 
-// initialize variable to toggle between add and remove items mode
+// Initialize a variable to toggle between add and remove items mode
 bool removeItem = true;
 
-//
+// Initialize a flag to determine whether the configuration settings have been modified and need to be saved
 bool shouldSaveConfig = false;
-char ws_server_val[21] = "192.168.x.x";
+
+// Initialize variables to store the WebSocket server address, port, and path
+char ws_server_val[21] = "192.168.x.x";  // Replace "x.x" with the actual IP address of the WebSocket server
 int ws_server_port_val = 1880;
 char ws_server_path_val[41] = "/ws";
 
 void setup() {
-  // initialize the Serial Monitor
+  // Initialize the Serial Monitor
   Serial.begin(115200);
 
+  // Initialize the I2C communication and the LCD display
   Wire.begin();
   lcd.init();
   lcd.backlight();
 
-  // set the remove item button pin as input with pull-up resistor enabled
+  // Set the pin for the remove item button as input with pull-up resistor enabled
   pinMode(REMOVE_Item_Btn, INPUT_PULLUP);
 
+  // Set the pin 2 as output
   pinMode(2, OUTPUT);
 
-  // initialize the software serial connection for the RFID reader
+  // Initialize the software serial connection for the RFID reader
   RFID_Serial.begin(9600, SWSERIAL_8N1, RFID_Serial_RX, RFID_Serial_TX, false);
 
-  // check if the software serial connection was successfully initialized
+  // Check if the software serial connection was successfully initialized
   if (!RFID_Serial) {
     Serial.println("Invalid EspSoftwareSerial pin configuration, check config");
+    // If software serial connection fails, print an error message and loop infinitely
     while (1) {
       delay(1000);
     }
   }
 
-  bool forceConfig = false;  // Change to true when testing to force configuration every time we run
+  // Set the initial value of forceConfig to false
+  bool forceConfig = false;
 
+  // Load the configuration from the SPIFFS file system
   bool spiffsSetup = loadConfigFile();
+
+  // If the configuration file does not exist, force the configuration mode
   if (!spiffsSetup) {
     Serial.println(F("Forcing config mode as there is no saved config"));
     forceConfig = true;
   }
 
-  WiFi.mode(WIFI_STA);  // explicitly set mode, esp defaults to STA+AP
+  // Set the ESP8266 WiFi mode to Station mode
+  WiFi.mode(WIFI_STA);
+
+  // Setup the WiFiManager library to handle WiFi configuration and connection
   setupWiFiManager();
 
-  // server address, port and URL
+  // Set the server address, port, and URL for the WebSocket connection
   webSocket.begin(ws_server_val, ws_server_port_val, ws_server_path_val);
 
-  // event handler
+  // Set the WebSocket event handler
   webSocket.onEvent(webSocketEvent);
 
-  // try ever 5000 again if connection has failed
+  // Try to reconnect to the WebSocket server every 5000ms if connection has failed
   webSocket.setReconnectInterval(5000);
 
-  // start heartbeat (optional)
-  // ping server every 15000 ms
-  // expect pong from server within 3000 ms
-  // consider connection disconnected if pong is not received 2 times
+  // Start the WebSocket heartbeat to keep the connection alive
+  // Ping the server every 15000ms and expect a response within 3000ms
+  // If the response is not received 2 times, consider the connection as disconnected
   webSocket.enableHeartbeat(15000, 3000, 2);
 
-
+  // Print the WebSocket server information to the Serial Monitor
   Serial.print("WS server: ");
   Serial.println(ws_server_val);
   Serial.print("WS server port: ");
@@ -118,10 +131,12 @@ void setup() {
   Serial.print("WS server path: ");
   Serial.println(ws_server_path_val);
 
+  // Display a testing message on the LCD display for 1 second
   lcd.setCursor(0, 0);
   lcd.print("Testing...");
   delay(1000);
 }
+
 
 void loop() {
   webSocket.loop();
@@ -176,80 +191,89 @@ void handleIncomingData(uint8_t *json) {
 
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
   switch (type) {
-    case WStype_DISCONNECTED:
+    case WStype_DISCONNECTED:  // The WebSocket connection has been disconnected
       Serial.printf("[WSc] Disconnected!\n");
       break;
-    case WStype_CONNECTED:
+    case WStype_CONNECTED:  // A WebSocket connection has been established
       {
         Serial.printf("[WSc] Connected to url: %s\n", payload);
         // send message to server when Connected
-        // webSocket.sendTXT(("{ \"command\": \"connected\", \"device\": \"" + String(device_name_val) + "\"}").c_str());
+        webSocket.sendTXT(("{ \"command\": \"connected\"}").c_str());  // Send a text message to the server after a connection has been established
       }
       break;
-    case WStype_TEXT:
+    case WStype_TEXT:  // A text message has been received from the server
       Serial.printf("[WSc] get text: %s\n", payload);
-      handleIncomingData(payload);
+      handleIncomingData(payload);  // Call the handleIncomingData function to process the received data
       break;
-    case WStype_BIN:
+    case WStype_BIN:  // A binary message has been received from the server
       Serial.printf("[WSc] get binary length: %u\n", length);
-      hexdump(payload, length);
+      hexdump(payload, length);  // Print the binary data in hexadecimal format
       break;
-    case WStype_PING:
+    case WStype_PING:  // A ping message has been received from the server
       // pong will be send automatically
       Serial.printf("[WSc] get ping\n");
       break;
-    case WStype_PONG:
+    case WStype_PONG:  // A pong message has been received from the server
       // answer to a ping we send
       Serial.printf("[WSc] get pong\n");
       break;
   }
 }
 
-
 void saveConfigFile() {
   Serial.println(F("Saving configuration..."));
 
-  // Create a JSON document
+  // Create a JSON document with a size of 512 bytes
   StaticJsonDocument<512> json;
-  json["ws_server"] = ws_server_val;
-  json["ws_server_port"] = ws_server_port_val;
-  json["ws_server_path"] = ws_server_path_val;
 
-  // Open config file
+  // Set the values of the JSON object
+  json["ws_server"] = ws_server_val;            // WebSocket server address
+  json["ws_server_port"] = ws_server_port_val;  // WebSocket server port
+  json["ws_server_path"] = ws_server_path_val;  // WebSocket server path
+
+  // Open the configuration file for writing
   File configFile = SPIFFS.open(JSON_CONFIG_FILE, "w");
   if (!configFile) {
     Serial.println("failed to open config file for writing");
   }
 
-  // Serialize JSON data to write to file
-  serializeJsonPretty(json, Serial);
-  if (serializeJson(json, configFile) == 0) {
+  // Serialize the JSON data to write to the file
+  serializeJsonPretty(json, Serial);           // Print the JSON data to the serial monitor for debugging
+  if (serializeJson(json, configFile) == 0) {  // Write the JSON data to the file
     Serial.println(F("Failed to write to file"));
   }
-  // Close file
+
+  // Close the file
   configFile.close();
 }
 
 // Load existing configuration file
 bool loadConfigFile() {
+
   // Uncomment if we need to format filesystem
   // SPIFFS.format();
 
-  // Read configuration from FS json
+  // Mount the SPIFFS file system
   Serial.println("Mounting File System...");
+  if (SPIFFS.begin()) { // If mounted successfully
 
-  // May need to make it begin(true) first time you are using SPIFFS
-  if (SPIFFS.begin()) {
     Serial.println("mounted file system");
+
+    // Check if the configuration file exists
     if (SPIFFS.exists(JSON_CONFIG_FILE)) {
-      // The file exists, reading and loading
+
+      // The file exists, so read and load it
       Serial.println("reading config file");
       File configFile = SPIFFS.open(JSON_CONFIG_FILE, "r");
       if (configFile) {
         Serial.println("Opened configuration file");
+
+        // Parse the JSON data from the configuration file
         StaticJsonDocument<512> json;
         DeserializationError error = deserializeJson(json, configFile);
         serializeJsonPretty(json, Serial);
+
+        // If the JSON data was parsed successfully, load the values
         if (!error) {
           Serial.println("Parsing JSON");
 
@@ -257,6 +281,7 @@ bool loadConfigFile() {
           ws_server_port_val = json["ws_server_port"].as<int>();
           strcpy(ws_server_path_val, json["ws_server_path"]);
           return true;
+
         } else {
           // Error loading JSON data
           Serial.println("Failed to load json config");
@@ -267,9 +292,10 @@ bool loadConfigFile() {
     // Error mounting file system
     Serial.println("Failed to mount FS");
   }
+
+  // Return false if there was an error
   return false;
 }
-
 
 // Callback notifying us of the need to save configuration
 void saveConfigCallback() {
@@ -288,14 +314,15 @@ void configModeCallback(WiFiManager *myWiFiManager) {
   Serial.println(WiFi.softAPIP());
 }
 
-
+// Initialize WiFiManager
 void setupWiFiManager() {
   // wm.resetSettings(); // reset settings - wipe stored credentials for testing
   wm.setSaveConfigCallback(saveConfigCallback);  // Set config save notify callback
   wm.setAPCallback(configModeCallback);          // Set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
   wm.setDebugOutput(false);                      //Disable debugging
-  wm.setDarkMode(true);                          // dark theme
+  wm.setDarkMode(true);                          // Enable dark theme
 
+  // Create WiFiManager parameters
   WiFiManagerParameter ws_server("ws_server", "Websocket Server Address", ws_server_val, 20);
   wm.addParameter(&ws_server);
 
@@ -306,10 +333,12 @@ void setupWiFiManager() {
   WiFiManagerParameter ws_server_path("ws_server_path", "Websocket Server Path", ws_server_path_val, 40);
   wm.addParameter(&ws_server_path);
 
+  // Add custom JavaScript to the WiFiManager page
   String custom_js_string = "<script>document.getElementById('ws_server_port').type = 'number';</script>";
   WiFiManagerParameter custom_js(custom_js_string.c_str());
   wm.addParameter(&custom_js);
 
+  // Connect to WiFi
   bool res;
   String AP_NAME = SSID;
   res = wm.autoConnect(AP_NAME.c_str(), PASSWORD);  // password protected ap
@@ -322,14 +351,15 @@ void setupWiFiManager() {
     Serial.println("connected...yeey :)");
   }
 
+  // Print WiFi information
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-
+  // Update parameters with values from WiFiManager
   strncpy(ws_server_val, ws_server.getValue(), sizeof(ws_server_val));                 // Copy the string value
-  ws_server_port_val = atoi(ws_server_port.getValue());                                //Convert the number value
+  ws_server_port_val = atoi(ws_server_port.getValue());                                // Convert the number value
   strncpy(ws_server_path_val, ws_server_path.getValue(), sizeof(ws_server_path_val));  // Copy the string value
 
   Serial.print("WS server: ");
@@ -338,7 +368,8 @@ void setupWiFiManager() {
   Serial.println(ws_server_port.getValue());
   Serial.print("WS server path: ");
   Serial.println(ws_server_path.getValue());
-  // Save the custom parameters to FS
+
+  // Save the custom parameters to file system if they have changed
   if (shouldSaveConfig) {
     saveConfigFile();
   }
